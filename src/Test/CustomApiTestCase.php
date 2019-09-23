b94bdabed73dd9d9f0965aa3ecd9975ad733a317
Guilherme Fontans <guilherme.fontans@gmail.com>
2019-09-22 22:00:17 -0300

    Created custom setup for tests

---
+++ src/Test/CustomApiTestCase.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Test;
+
+use App\ApiPlatform\Test\ApiTestCase;
+use App\ApiPlatform\Test\Client;
+use App\Entity\User;
+
+/**
+ * Class CustomApiTestCase
+ * @package App\Test
+ */
+class CustomApiTestCase extends ApiTestCase
+{
+
+    /**
+     * @param string $email
+     * @param string $password
+     * @return User
+     */
+    protected function createUser(string $email, string $password): User
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUsername(substr($email, 0, strpos($email, '@')));
+        $encoded = self::$container->get('security.password_encoder')
+            ->encodePassword($user, $password);
+        $user->setPassword($encoded);
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+        return $user;
+    }
+
+    /**
+     * @param Client $client
+     * @param string $email
+     * @param string $password
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    protected function logIn(Client $client, string $email, string $password)
+    {
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => $email,
+                'password' => $password
+            ]
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+    }
+
+    /**
+     * @param Client $client
+     * @param string $email
+     * @param string $password
+     * @return User
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    protected function createUserAndLogin(Client $client, string $email, string $password): User
+    {
+        $user = $this->createUser($email, $password);
+        $this->logIn($client, $email, $password);
+        return $user;
+    }
+}
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -2,49 +2,20 @@
 
 namespace App\Tests\Functional;
 
-use App\ApiPlatform\Test\ApiTestCase;
-use App\Entity\User;
-use Doctrine\ORM\EntityManager;
+use App\Test\CustomApiTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
-class CheeseListingResourceTest extends ApiTestCase
+/**
+ * Class CheeseListingResourceTest
+ * @package App\Tests\Functional
+ */
+class CheeseListingResourceTest extends CustomApiTestCase
 {
     use ReloadDatabaseTrait;
 
     public function testCreateCheeseListing()
     {
         $client = self::createClient();
-        $client->request('POST','/api/cheeses', [
-            'headers' => [
-                'Content-type' => 'application/json'
-            ],
-            'body' => []
-        ]);
-
-        $this->assertResponseStatusCodeSame(400);
-
-        $user = new User();
-        $user->setEmail("teste@teste.com");
-        $user->setUsername("teste@teste.com");
-        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$t2qjiuDPu38QiLh87eUsug$tviMBY+I/4b4bMu9xEW9pUCzQFAMXaDMQG2BYcHAEY8');
-
-        /**
-         * @var EntityManager
-         */
-        $em = self::$container->get('doctrine')->getManager();
-        $em->persist($user);
-        $em->flush();
-
-        $client->request('POST','/login', [
-            'headers' => [
-                'content-type' => 'application/json'
-            ],
-            'json' => [
-                'email' => 'teste@teste.com',
-                'password' => "123"
-            ]
-        ]);
-
-        $this->assertResponseStatusCodeSame(204);
+        $this->createUserAndLogin($client, 'teste@teste.com', '123');
     }
 }
